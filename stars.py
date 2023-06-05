86d43a19b4f466095a43c9270c6ccde8700679a8
Pranav Kulkarni <62786537+koolkee-pranav@users.noreply.github.com>
2023-06-05 13:53:17 +0000

    imported sys

--- stars.py
+++ stars.py
@@ -1,7 +1,10 @@
+#!/usr/bin/python3
+import sys
 
-print("Python program to print stars (*)")
+print("Python program to print stars *")
 
-howManyStars = input("How many stars do you want to print? ")
+howManyStars = input("Hey you! How many stars do you want to print? ")
 for i in range(int(howManyStars)):
     print("*")
-                     
+
+sys.exit()
