7e4b34f22e0ec9482559ff0ccc587ca799208e60
Pranav Kulkarni <62786537+koolkee-pranav@users.noreply.github.com>
2023-06-05 13:45:57 +0000

    added shebang

--- name.py
+++ name.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
+import sys
 
 print("My name is Pranav Kulkarni.")
 print("I was born January 1, 1970.")
 print("My favorite color is Blue.")
-
 print("By the great Amogh!!!!!!!!!!!!")
+
+sys.exit()
